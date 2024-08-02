d065d1fe3af76f9ea8a179e9361c70a1c1eb51a8
Anna Rychkova <134734366+AnnaRychkova1@users.noreply.github.com>
2024-08-02 12:34:54 +0100

    Update contactsOps.js

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://660da8476ddfa2943b34f519.mockapi.io';
+axios.defaults.baseURL = 'https://669908cb2069c438cd711199.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
